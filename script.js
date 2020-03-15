fc74118d7a6cc88f2e6fa069f3b3bb996ecb3712
vik-prog <60511526+vik-prog@users.noreply.github.com>
2020-03-15 18:25:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+// question 2
+console.log(petArray[1]);
+// question 3
+petArray.push("sheep");
+// question 4
+console.log(petArray.length);
+// question 5
+var catObject = {
+    name: "Pus",
+    colour: "white",
+    age: 1
+};
+// question 6
+console.log(catObject.age);
+// question 7
+var catArray = [
+    {
+        name: "Pus",
+        colour: "white",
+        age: 1
+    }
+];
+// question 8
+for (var i = 0; i < catArraay.length; i++) {
+    console.log(catArray[i].name);
+    console.log(catArray[i].colour);
+    console.log(catArray[i].age);
+}
+// question 9
+function logToConsole(consoleMessage) {
+    console.log(consoleMessage);
+}
+// question 10
+logToConsole("donkey");
